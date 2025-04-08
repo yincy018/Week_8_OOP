76b766c41d105cbeb5e2e3607dcc3ed3d703be47
yincy018 <yincy018@mymail.unisa.edu.au>
2025-04-08 14:22:18 +0930

    Implemented c_to_f function

--- temperature_converter.py
+++ temperature_converter.py
@@ -5,3 +5,10 @@ def f_to_c(f):
         return (f-32)*(5/9)
     else:
         raise TypeError("Only int or float is allowed.")
+    
+def c_to_F(c):
+    '''Convert argument temperature from celsius to fahrenheit.'''
+    if (isinstance(c, int) or isinstance(c, float)):
+        return (c* 9/5) + 32
+    else:
+        raise TypeError("Only int or float is allowed.")
